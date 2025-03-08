bbed54c90ec1b76a3e4f8387048c7ea5d0bfb483
F978566 <77454354+F978566@users.noreply.github.com>
2025-03-08 22:03:21 +0300

    change ScheduleService

--- application/services/schedule.py
+++ application/services/schedule.py
@@ -1,5 +1,6 @@
 from typing import List
 from domain.entities.problem import ProblemEntity
+from domain.entities.problem_list import ProblemListEntity
 from domain.entities.week_schedule import WeekScheduleEntity
 from domain.repositories.schedule import ScheduleRepository
 
@@ -7,7 +8,7 @@ from domain.repositories.schedule import ScheduleRepository
 class ScheduleProblemsService:
     def __init__(self, schedule_repository: ScheduleRepository):
         self.schedule_repository = schedule_repository
-        self.problems = []
+        self.problems: dict[str, ProblemListEntity] = {}
 
     def find_problems(self):
         schedule_list = self.schedule_repository.get_schedule_list()
@@ -18,20 +19,28 @@ class ScheduleProblemsService:
                 self.late_start(week_schedule)
 
     def big_workload_differences(
-        self, schedule: WeekScheduleEntity
-    ) -> List[ProblemEntity]:
+        self, schedule: tuple[WeekScheduleEntity, WeekScheduleEntity]
+    ):
         odd_differences = self.workload_differences(schedule[0])
         even_differences = self.workload_differences(schedule[1])
 
+        problems_list = ProblemListEntity(
+            problems=[], full_group_title=schedule[0].full_group_title
+        )
+
         if odd_differences >= 4 or even_differences >= 4:
-            self.problems.append(
+            problems_list.problems.append(
                 ProblemEntity(
                     name="Big workload difference",
                     description=f"The differences between the busiest and the least busy day in one week is {odd_differences} classes",
+                    full_group_title=schedule[0].full_group_title,
                 )
             )
 
-        return self.problems
+        if problems_list.full_group_title in self.problems:
+            self.problems[problems_list.full_group_title].problems.append(problems_list)
+        else:
+            self.problems[problems_list.full_group_title] = problems_list
 
     def workload_differences(self, week_schedule: WeekScheduleEntity):
         max_classes = 0
@@ -57,6 +66,10 @@ class ScheduleProblemsService:
             x.hour * 60 + x.minute - x.hour * 60 - y.minute
         )
 
+        problems_list = ProblemListEntity(
+            problems=[], full_group_title=week_schedule.full_group_title
+        )
+
         for day in week_schedule.schedule:
             if len(day.subjects) <= 1:
                 continue
@@ -69,24 +82,40 @@ class ScheduleProblemsService:
                     != get_auditorium(prev_subject.location)
                     and time_difference(subject.time_start, prev_subject.time_end) <= 10
                 ):
-                    self.problems.append(
+                    problems_list.problems.append(
                         ProblemEntity(
                             name="Long commute",
                             description=f"A student may be late for their next class if they have to walk from {prev_subject.location} to {subject.location} in 10 minute day - {day.day_of_week.name} week - {week_schedule.week.name}",
+                            full_group_title=week_schedule.full_group_title,
                         )
                     )
                 prev_subject = subject
 
+        if problems_list.full_group_title in self.problems:
+            self.problems[problems_list.full_group_title].problems.append(problems_list)
+        else:
+            self.problems[problems_list.full_group_title] = problems_list
+
     def late_start(self, week_schedule: WeekScheduleEntity):
+        problems_list = ProblemListEntity(
+            problems=[], full_group_title=week_schedule.full_group_title
+        )
+
         for day in week_schedule.schedule:
             if len(day.subjects) == 0:
                 continue
 
             first_class = sorted(day.subjects, key=lambda x: x.time_start.hour)[0]
             if first_class.time_start.hour < 12:
-                self.problems.append(
+                problems_list.problems.append(
                     ProblemEntity(
                         name="Late start",
                         description=f"It is better to start the day early {first_class.time_start.hour} in the morning - {day.day_of_week.name} week - {week_schedule.week.name}",
+                        full_group_title=week_schedule.full_group_title,
                     )
                 )
+
+        if problems_list.full_group_title in self.problems:
+            self.problems[problems_list.full_group_title].problems.append(problems_list)
+        else:
+            self.problems[problems_list.full_group_title] = problems_list
